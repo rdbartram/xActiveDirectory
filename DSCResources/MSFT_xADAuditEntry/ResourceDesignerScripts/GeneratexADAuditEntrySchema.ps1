9378665cc78aa2a6f23198b8354e0ad9ab1f7b66
Ryan Bartram <ryan@bartram.ch>
2018-01-03 14:56:27 +0100

    Update to remove none as valid audit type

--- DSCResources/MSFT_xADAuditEntry/ResourceDesignerScripts/GeneratexADAuditEntrySchema.ps1
+++ DSCResources/MSFT_xADAuditEntry/ResourceDesignerScripts/GeneratexADAuditEntrySchema.ps1
@@ -2,7 +2,7 @@ New-xDscResource -Name MSFT_xADAuditEntry -FriendlyName xADAuditEntry -ModuleNam
     New-xDscResourceProperty -Name Name -Type string -Attribute Key -Description "A string uniquely identifiable within the configuration"
     New-xDscResourceProperty -Name Identity -Type string -Attribute Required
     New-xDscResourceProperty -Name Path -Type string -Attribute Required
-    New-xDscResourceProperty -Name AuditType -Type string -Attribute Write -ValidateSet @("Failure", "None", "Success")
+    New-xDscResourceProperty -Name AuditType -Type string[] -Attribute Write -ValidateSet @("Failure", "Success")
     New-xDscResourceProperty -Name Permission -Type string[] -Attribute Required -ValidateSet @("CreateChild", "DeleteChild", "ListChildren", "Self", "ReadProperty", "WriteProperty", "DeleteTree", "ListObject", "ExtendedRight", "Delete", "ReadControl", "GenericExecute", "GenericWrite", "GenericRead", "WriteDacl", "WriteOwner", "GenericAll", "Synchronize", "AccessSystemSecurity")
     New-xDscResourceProperty -Name AppliesTo -Type string -Attribute Write -ValidateSet @("All", "Children", "Descendents", "None", "SelfAndChildren")
     New-xDscResourceProperty -Name ExtendedPermission -Type string[] -Attribute Write
